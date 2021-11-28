bb9e7b29f3760407e4452e971a72517154e9e5c5
Sophia <sophkiani@gmail.com>
2021-11-28 10:29:08 -0600

    updated mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,15 @@
+
+// Require mongoose
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/e-shop', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/e-shop', {
+  // useNewUrlParser: true,
+  // useUnifiedTopology: true,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
+  }
+);
 
+// Explore mongoose
 module.exports = mongoose.connection;
-
